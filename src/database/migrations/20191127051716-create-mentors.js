3b54aa1adb8ae64c02e5bc4ff442681718b8e109
SkullDarth <yan.brito@hotmail.com>
2019-11-27 02:26:31 -0300

    Created Mentor migrate and Insomnia file

--- src/database/migrations/20191127051716-create-mentors.js
+++ src/database/migrations/20191127051716-create-mentors.js
@@ -1,23 +1,38 @@
-'use strict';
-
 module.exports = {
+  // Quando a migration for executada vai criar as tabelas abaixo
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  // Rollback da migration
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
